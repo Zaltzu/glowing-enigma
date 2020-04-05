d22553b5fdd27c5eec25ccb12f18b1846595eb5c
Zaltzu <56794155+Zaltzu@users.noreply.github.com>
2020-04-05 22:14:19 +0300

    Add files via upload

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"tyokalenteri","l":"Asiakas"},{"p":"tyokalenteri","l":"Henkilo"},{"p":"tyokalenteri","l":"Kalenteri"},{"p":"tyokalenteri","l":"Kategoria"},{"p":"tyokalenteri","l":"Tyo"},{"p":"tyokalenteri","l":"Tyokalenteri"},{"p":"tyokalenteri","l":"Tyontekija"}]
